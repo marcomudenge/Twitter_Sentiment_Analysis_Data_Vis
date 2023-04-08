c652da1e7201d0d5ddb5638d64aef437a2803e01
Marco Mudenge <mudmarco@gmail.com>
2023-04-07 21:48:54 -0400

    Radar chart vizualization fist commit

--- src/app.py
+++ src/app.py
@@ -9,16 +9,27 @@ App initialization file
 """
 
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 from dash.dependencies import Input, Output, State
 
 from datetime import date
 
 import plotly.graph_objects as go #move into viz files ?
+import pandas as pd
+
+import preprocess, radar
 
 app = dash.Dash(__name__)
 app.title = "" #TBD
+
+df_stats = pd.read_csv('src/assets/df_stats.csv')
+df_tweets = pd.read_csv('src/assets/df_tweets.csv')
+
+df_stats = preprocess.convert_dates(df_stats)
+df_tweets = preprocess.convert_dates(df_tweets)
+
+radar_fig = radar.get_radar_figure(df_stats)
 
 app.layout = html.Div(className='content', children = [
     html.Header(children=[
@@ -35,6 +46,20 @@ app.layout = html.Div(className='content', children = [
             dcc.DatePickerRange(id='my-date-picker-range',  #selecteur de date pour la viz 4
                                 min_date_allowed=date(2021, 3, 1),
                                 max_date_allowed=date(2021, 8, 1)
+            )
+        ]),
+        html.Div(className='viz-container-1', children=[
+        dcc.Graph(
+            id='radar-graph',
+            className='graph',
+            figure=radar_fig,
+            config=dict(
+                scrollZoom=False,
+                showTips=False,
+                showAxisDragHandles=False,
+                doubleClick=False,
+                displayModeBar=False
+                )
             )
         ])
     ])
